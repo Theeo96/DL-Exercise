24df79e4af760d2cf92de2c2c0b50751f0d08c7a
theeo96 <eth1004p@gmail.com>
2025-11-05 18:01:16 +0900

    251105 exer & modify

--- notebooks/DL3_20241006/ch04/two_layer_net_backpropa.py
+++ notebooks/DL3_20241006/ch04/two_layer_net_backpropa.py
@@ -6,8 +6,10 @@ current_dir = os.path.dirname(__file__)
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
-from common.functions import *
+import numpy as np
+from common.layers import *
 from common.gradient import numerical_gradient
+from collections import OrderedDict
 
 class TwoLayerNet :
 
@@ -19,26 +21,30 @@ class TwoLayerNet :
         self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
         self.params['b2'] = np.zeros(output_size)
 
+        # 계층 생성
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+        
+        self.lastLayer = SoftmaxWithLoss()
+
     def predict(self, x) :
-        W1, W2 = self.params['W1'], self.params['W2']
-        b1, b2 = self.params['b1'], self.params['b2']
+        for layer in self.layers.values() :
+            x = layer.forward(x)
 
-        a1 = np.dot(x, W1) + b1
-        z1 = sigmoid(a1)
-        a2 = np.dot(z1, W2) + b2
-        y = softmax(a2)
-
-        return y
+        return x
     
     def loss(self, x, t) :
         y = self.predict(x)
 
-        return cross_entropy_error(y, t)
+        return self.lastLayer.forward(y, t)
     
     def accuracy(self, x, t) :
         y = self.predict(x)
         y = np.argmax(y, axis=1)
-        t = np.argmax(t, axis=1)
+        
+        if t.ndim != 1 : t = np.argmax(t, axis=1)
 
         accuracy = np.sum(y == t) / float(x.shape[0])
 
@@ -56,27 +62,22 @@ class TwoLayerNet :
         return grads
 
     def gradient(self, x, t) :
-        W1, W2 = self.params['W1'], self.params['W2']
-        b1, b2 = self.params['b1'], self.params['b2']
-        grads = {}
-
-        batch_num = x.shape[0]
 
         # forward
-        a1 = np.dot(x, W1) + b1
-        z1 = sigmoid(a1)
-        a2 = np.dot(z1, W2) + b2
-        y = softmax(a2)
+        self.loss(x, t)
 
         # backward
+        dout = 1
+        dout = self.lastLayer.backward(dout)
 
-        dy = (y - t) / batch_num
-        grads['W2'] = np.dot(z1.T, dy)
-        grads['b2'] = np.sum(dy, axis=0)
+        layers = list(self.layers.values())
+        layers.reverse()
 
-        da1 = np.dot(dy, W2.T)
-        dz1 = sigmoid_grad(a1) * da1
-        grads['W1'] = np.dot(x.T, dz1)
-        grads['b1'] = np.sum(dz1, axis=0)
+        for layer in layers :
+            dout = layer.backward(dout)
+
+        grads = {}
+        grads['W1'], grads['b1'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
+        grads['W2'], grads['b2'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
 
         return grads
